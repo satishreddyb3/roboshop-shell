db1f0c04d7c542a0329cba3f3daf2e11c30548b4
satishreddyb3 <satishreddyb3@gmail.com>
2024-06-15 17:13:31 +0530

    mongodb upddated

---
+++ mongodb.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[32m"
+N="\e[0m" 
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+echo "script started executing at $TIMESTAMP" &>> $LOGFILE
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2...$R FAILED $N"
+        exit 1
+    else
+        echo -e "$2...$G SUCCESS $N"
+    fi
+}
+if [ $ID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1 # you can give other than 0
+else
+    echo "You are root user"
+fi
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
+
+
+VALIDATE $? "Copied MongoDB Repo"
+dnf install mongodb-org -y &>> $LOGFILE
+VALIDATE $? "installin mongoDb"
+systemctl enable mongod &>> $LOGFILE
+VALIDATE $? "enable mongoDb"
+systemctl start mongod &>> $LOGFILE
+VALIDATE $? "start mongoDb"
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>> $LOGFILE
+VALIDATE $? "remote access to MongoDB"
+systemctl restart mongod &>> $LOGFILE
+VALIDATE $? "Restart mongoDB"
